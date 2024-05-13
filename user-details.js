d30bff23e460b0258563bfcecdb731a9f68ffb3c
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2024-05-13 17:41:36 +0300

    added button getPosts

--- user-details.js
+++ user-details.js
@@ -1,5 +1,57 @@
 window.onload = function () {
   const currentUserId = JSON.parse(localStorage.getItem("currentUserId"));
+
+  const getUserPosts = (commentsButton) => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${currentUserId}/posts`)
+      .then((response) => response.json())
+      .then((comments) => {
+        if (comments) {
+          const usersPosts = document.querySelector(".users-posts");
+          usersPosts.classList.add("user-posts-wrapper");
+
+          const userPostsTitle = document.createElement("h1");
+          userPostsTitle.classList.add("user-posts-title");
+          userPostsTitle.textContent = "User Comments";
+
+          usersPosts.appendChild(userPostsTitle);
+
+          const openPosts = () => {
+            usersPosts.classList.toggle("hidden");
+            commentsButton.textContent = "Hiden";
+          };
+
+          const renderPosts = (clasName, comment, index) => {
+            const userComment = document.createElement("div");
+            userComment.classList.add("user-comments");
+            userComment.addEventListener("click", () => {
+              localStorage.setItem(
+                "currentCommentId",
+                JSON.stringify(comment.id)
+              );
+              window.location.href = "user-comment.html";
+            });
+            const element = document.createElement("h1");
+            const number = document.createElement("span");
+            element.classList.add(clasName);
+            element.textContent = `${index + 1}:  ${
+              comment.title ? comment.title : "N/A"
+            }`;
+            userComment.appendChild(element);
+            usersPosts.appendChild(userComment);
+          };
+
+          comments.map((comment, index) => {
+            renderPosts("text", comment, index);
+          });
+          commentsButton.addEventListener("click", () => {
+            openPosts();
+          });
+        } else {
+          console.error("Current users comments data not found.");
+        }
+      });
+  };
+
   fetch(`https://jsonplaceholder.typicode.com/users/${currentUserId}`)
     .then((response) => response.json())
     .then((currentUser) => {
@@ -75,56 +127,29 @@ window.onload = function () {
           "label"
         );
 
+        const buttonWrapper = document.createElement("div");
+
         const backButton = document.createElement("button");
         backButton.classList.add("userDetail-button");
         backButton.textContent = "Back";
         backButton.addEventListener("click", () => {
           window.location.href = "index.html";
         });
-        userDetails.appendChild(backButton);
+
+        const commentsButton = document.createElement("button");
+        commentsButton.classList.add("userDetail-button");
+        commentsButton.textContent = "Comments";
+        commentsButton.addEventListener("click", () => {
+          getUserPosts(commentsButton);
+        });
+
+        buttonWrapper.appendChild(backButton);
+        buttonWrapper.appendChild(commentsButton);
+
+        userDetails.appendChild(buttonWrapper);
       } else {
         console.error("Current user data not found.");
       }
     })
     .catch((error) => console.error("Error fetching current user:", error));
-  fetch(`https://jsonplaceholder.typicode.com/users/${currentUserId}/posts`)
-    .then((response) => response.json())
-    .then((comments) => {
-      if (comments) {
-        const usersPosts = document.querySelector(".users-posts");
-        usersPosts.classList.add("user-posts-wrapper");
-
-        const userPostsTitle = document.createElement("h1");
-        userPostsTitle.classList.add("user-posts-title");
-        userPostsTitle.textContent = "User Comments";
-
-        usersPosts.appendChild(userPostsTitle);
-
-        const renderPosts = (clasName, comment, index) => {
-          const userComment = document.createElement("div");
-          userComment.classList.add("user-comments");
-          userComment.addEventListener("click", () => {
-            localStorage.setItem(
-              "currentCommentId",
-              JSON.stringify(comment.id)
-            );
-            window.location.href = "user-comment.html";
-          });
-          const element = document.createElement("h1");
-          const number = document.createElement("span");
-          element.classList.add(clasName);
-          element.textContent = `${index + 1}:  ${
-            comment.title ? comment.title : "N/A"
-          }`;
-          userComment.appendChild(element);
-          usersPosts.appendChild(userComment);
-        };
-
-        comments.map((comment, index) => {
-          renderPosts("text", comment, index);
-        });
-      } else {
-        console.error("Current users comments data not found.");
-      }
-    });
 };

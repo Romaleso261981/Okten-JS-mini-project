63deddf8d7d61ff2cc7561ea7723824e632adb0d
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2024-05-13 19:05:21 +0300

    some bag fix

--- user-details.js
+++ user-details.js
@@ -39,46 +39,33 @@ window.onload = function () {
         };
 
         renderFunction("text", currentUser.id, "ID", "label");
-
         renderFunction("text", currentUser.address.city, "City", "label");
-
         renderFunction("text", currentUser.company.bs, "Company BS", "label");
-
         renderFunction("text", currentUser.name, "Name", "label");
-
         renderFunction("text", currentUser.address.street, "Street", "label");
-
         renderFunction(
           "text",
           currentUser.company.catchPhrase,
           "Company Catch Phrase",
           "label"
         );
-
         renderFunction("text", currentUser.username, "Username", "label");
-
         renderFunction(
           "text",
           currentUser.company.name,
           "Company Name",
           "label"
         );
-
         renderFunction("text", currentUser.address.suite, "Suite", "label");
-
         renderFunction("text", currentUser.website, "Website", "label");
-
         renderFunction(
           "text",
           currentUser.address.geo.lat,
           "user-address-geo(lat)",
           "label"
         );
-
         renderFunction("text", currentUser.address.zipcode, "Zipcode", "label");
-
         renderFunction("text", currentUser.email, "Email", "label");
-
         renderFunction(
           "text",
           currentUser.address.geo.lng,
